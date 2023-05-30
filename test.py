418975a9d2141e3f672ada9152163479e74dd297
CorrosiveMoon <89851845+CorrosiveMoon@users.noreply.github.com>
2023-05-30 18:32:13 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,41 +1,41 @@
-import csv
-import json
+# import csv
+# import json
 
-def count_rows(filename = 'C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\neos.csv'):
-    with open(filename) as f:
-        reader = csv.reader(f)
-        data = list(reader)
-        row_count = len(data)
-        return row_count
+# def count_rows(filename = 'C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\neos.csv'):
+#     with open(filename) as f:
+#         reader = csv.reader(f)
+#         data = list(reader)
+#         row_count = len(data)
+#         return row_count
     
-print(count_rows())
+# print(count_rows())
 
-#how many neos have diamters in the data set
-def count_diameters(filename = 'C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\neos.csv'):
-    with open(filename):
-        reader = csv.DictReader(filename)
-        diameter = []
-        for row in reader:
-            diameter.append(row['diameter'])
-        return diameter
+# #how many neos have diamters in the data set
+# def count_diameters(filename = 'C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\neos.csv'):
+#     with open(filename):
+#         reader = csv.DictReader(filename)
+#         diameter = []
+#         for row in reader:
+#             diameter.append(row['diameter'])
+#         return diameter
     
-# print(count_diameters())
+# # print(count_diameters())
 
 
-def find_closest_neo():
-    with open('C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\cad.json') as f:
-        data = json.load(f)
+# def find_closest_neo():
+#     with open('C:\\Users\\MSI\\Desktop\\Near Earth Objects\\data\\cad.json') as f:
+#         data = json.load(f)
     
-    closest_neo = None
-    closest_distance = float('inf')
+#     closest_neo = None
+#     closest_distance = float('inf')
     
-    for neo in data['data']:
-        if neo['close_approach_date'].startswith('2015'):
-            distance = float(neo['close_approach_data'][0]['miss_distance']['astronomical'])
-            if distance < closest_distance:
-                closest_distance = distance
-                closest_neo = neo['designation']
+#     for neo in data['data']:
+#         if neo['close_approach_date'].startswith('2015'):
+#             distance = float(neo['close_approach_data'][0]['miss_distance']['astronomical'])
+#             if distance < closest_distance:
+#                 closest_distance = distance
+#                 closest_neo = neo['designation']
     
-    return closest_neo
+#     return closest_neo
 
-print(find_closest_neo())
+# print(find_closest_neo())
--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from helpers import datetime_to_str
 
 
 def write_to_csv(results, filename):
@@ -26,6 +27,30 @@ def write_to_csv(results, filename):
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    # with open(filename, 'w', newline= '') as outfile:
+        # writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        # writer.writeheader()
+        # for approach in results:
+        #     neo = approach.neo
+        #     row = {
+        #         'datetime_utc': approach.time_str,
+        #         'distance_au': approach.distance,
+        #         'velocity_km_s': approach.velocity,
+        #         'designation': neo.designation,
+        #         'name': neo.name or '',
+        #         'diameter_km': neo.diameter or '',
+        #         'potentially_hazardous': str(neo.hazardous) if neo.hazardous is not None else ''
+        #     }
+        #     writer.writerow(row)
+    with open(filename, "w", newline="") as outfile:
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for result in results:
+                content = {**result.serialize(), **result.neo.serialize()}
+                content["name"] = content["name"] if content["name"] is not None else ""
+                content["potentially_hazardous"] = "True" if content["potentially_hazardous"] else "False"
+                writer.writerow(content)
+
 
 
 def write_to_json(results, filename):
@@ -40,3 +65,23 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+
+    json_list = []
+    for approach in results:
+        neo = approach.neo
+        entry = {
+            'datetime_utc': datetime_to_str(approach.time),
+            'distance_au': approach.distance,
+            'velocity_km_s': approach.velocity,
+            'neo': {
+                'designation': neo.designation,
+                'name': neo.name or '',
+                'diameter_km': neo.diameter or float('nan'),
+                'potentially_hazardous': neo.hazardous
+            }
+
+        }
+        json_list.append(entry)
+    with open(filename, 'w') as outfile:
+        json.dump(json_list, outfile, indent=2)
+
